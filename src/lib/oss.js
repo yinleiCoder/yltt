735bc46ea5cd421a695815377e3c8f675db9b931
yinlei <1099129793@qq.com>
2026-06-09 17:42:51 +0800

    fixed oss config error

--- src/lib/oss.js
+++ src/lib/oss.js
@@ -7,6 +7,7 @@ export function createOSSClient() {
     accessKeyId: process.env.OSS_ACCESS_KEY_ID,
     accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
     bucket: process.env.OSS_BUCKET,
+    secure: true,
   })
 }
 
